304d9104c5d5404fec7a89aa4cd17ee54052cb68
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-02-04 12:08:12 +0530

    add

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function generateTable() {
     // Get the user-entered number
     var number = document.getElementById("numberInput").value  * document.getElementById("numberInput2").value;
 
+    var divident = document.getElementById("numberInput").value;
+
+    var divisor = document.getElementById("numberInput2").value;
+
     // Validate the input
     if (!number || isNaN(number) || number <= 0) {
         alert("Please enter a valid positive number.");
@@ -9,9 +13,9 @@ function generateTable() {
     }
 
     // Generate the table
-    var tableHTML = "<h2>Table for " + number + "</h2><table border='1'><tr><th>Multiplier</th><th>Result</th></tr>";
+    var tableHTML = "<h2>Table for " + divident+"/"+ divisor + "</h2><table border='1'><tr><th>N</th><th>M</th><th>A</th><th>Q</th><th>Operation</th></tr>";
     for (var i = 1; i <= 10; i++) {
-        tableHTML += "<tr><td>" + i + "</td><td>" + (i * number) + "</td></tr>";
+        tableHTML += "<tr><td>" + i + "</td><td>" + (i * number) + "</td><td>" + i + "</td></tr>";
     }
     tableHTML += "</table>";
 
