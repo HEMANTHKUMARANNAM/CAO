ba4779d59833f2af741a03719ee95dd0641f928c
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-02-04 11:32:51 +0530

    add

---
+++ script.js
@@ -0,0 +1,20 @@
+function generateTable() {
+    // Get the user-entered number
+    var number = document.getElementById("numberInput").value;
+
+    // Validate the input
+    if (!number || isNaN(number) || number <= 0) {
+        alert("Please enter a valid positive number.");
+        return;
+    }
+
+    // Generate the table
+    var tableHTML = "<h2>Table for " + number + "</h2><table border='1'><tr><th>Multiplier</th><th>Result</th></tr>";
+    for (var i = 1; i <= 10; i++) {
+        tableHTML += "<tr><td>" + i + "</td><td>" + (i * number) + "</td></tr>";
+    }
+    tableHTML += "</table>";
+
+    // Display the table
+    document.getElementById("tableContainer").innerHTML = tableHTML;
+}
